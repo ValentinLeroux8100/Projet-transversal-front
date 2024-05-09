847b44d497cbcce9464d7afe5f284f5fe73e7921
ValentinLeroux8100 <74792769+ValentinLeroux8100@users.noreply.github.com>
2024-05-09 14:57:05 +0200

    bon là tous est bon

--- src/pages/acceuil/acceuil.tsx
+++ src/pages/acceuil/acceuil.tsx
@@ -6,7 +6,7 @@ export default function Acceuil() {
   return (
     <main className="flex flex-col gap-8 py-8 px-12">
       <section className="flex flex-col gap-8 ">
-        <div className="flex flex-row md:h-[300px] flex-wrap place-content-around gap-4 p-4">
+        <div className="flex flex-row flex-wrap place-content-evenly gap-4 p-4">
           <CarteCategorie></CarteCategorie>
           <CarteCategorie></CarteCategorie>
           <CarteCategorie></CarteCategorie>
@@ -16,7 +16,7 @@ export default function Acceuil() {
         <h2 className="text-center text-black text-5xl font-semibold font-body capitalize">
           Les Highlanders du moment
         </h2>
-        <div className="flex flex-row flex-wrap md:h-[300px]  place-content-around gap-4 p-4">
+        <div className="flex flex-row flex-wrap   place-content-around gap-4 p-4">
           <CarteProduit></CarteProduit>
           <CarteProduit></CarteProduit>
           <CarteProduit></CarteProduit>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#E6C077",
+      secondary: "#A58268",
+      background: "#FFFFFF",
+      valid: "#BFFFAF",
+      text: "#1B1A19",
+      secondaryText: "#6f6c6c",
+    },
     extend: {
       fontFamily: {
         body: ["Noto Sans", "sans-serif"],
+        logo: ["Orelega One"],
       },
     },
   },
