760d19556208ea04e8f01160f3833f21d5b7fc4c
ValentinLeroux8100 <74792769+ValentinLeroux8100@users.noreply.github.com>
2024-05-13 14:19:48 +0200

    refacto

--- src/pages/acceuil/acceuil.tsx
+++ src/pages/acceuil/acceuil.tsx
@@ -1,34 +1,19 @@
 import React from "react";
-import CarteProduit from "components/site/carteProduit/carteProduit";
-import CarteCategorie from "components/site/carteCategorie/carteCategorie";
+import ListeProduits from "components/site/listeProduits/ListeProduits";
+import ListeCategories from "components/site/listeCategories/listeCategories";
 import acceuilFixtureData from "fixture/acceuilFixture";
 
 export default function Acceuil() {
-  const data = acceuilFixtureData;
+  const { produits, categories } = acceuilFixtureData;
 
   return (
     <main className="flex flex-col gap-8 py-8 px-12">
       <section className="flex flex-col gap-8 ">
-        <div className="flex flex-row flex-wrap place-content-evenly gap-4 p-4">
-          {data.categories.map((categorie) => {
-            return (
-              <CarteCategorie
-                image={categorie.image}
-                nom={categorie.nom}
-              ></CarteCategorie>
-            );
-          })}
-        </div>
+        <ListeCategories categories={categories}></ListeCategories>
       </section>
       <section className="flex flex-col gap-8">
-        <h2 className="text-center text-black text-3xl font-semibold font-body capitalize">
-          Les Highlanders du moment
-        </h2>
-        <div className="flex flex-row flex-wrap place-content-around gap-4 p-4">
-          {data.produits.map((produit) => {
-            return <CarteProduit produit={produit}></CarteProduit>;
-          })}
-        </div>
+        <h2 className="titre-section">Les Highlanders du moment</h2>
+        <ListeProduits produits={produits}></ListeProduits>
       </section>
     </main>
   );
